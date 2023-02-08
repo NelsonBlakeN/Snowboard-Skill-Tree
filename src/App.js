6f2e95e7cce26bd57c09d04cb7a1000e9ec3f4fc
NelsonBlakeN <nelson.blaken@gmail.com>
2023-02-07 22:02:17 -0800

    Changes for initial skill tree deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  SkillTreeGroup,
+  SkillTree,
+  SkillProvider,
+  SkillType,
+  SkillGroupDataType,
+} from 'beautiful-skill-tree'
+import grabTricks from './grabTricks.json'
+import butterTricks from './butterTricks.json'
+import flipTricks from './flipTricks.json'
+import jibTricks from './jibTricks.json'
+import spinTricks from './spinTricks.json'
+import stallTricks from './stallTricks.json'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const allTricks = [
+        {
+            trickSet: grabTricks,
+            title: "Grabs",
+            treeId: "grabSkillTree",
+            description: "Skill tree for grab tricks"
+        },
+        {
+            trickSet: spinTricks,
+            title: "Spins",
+            treeId: "spinSkillTree",
+            description: "Skill tree for spin tricks"
+        },
+        {
+            trickSet: jibTricks,
+            title: "Jibs",
+            treeId: "jibSkillTree",
+            description: "Skill tree for jib tricks"
+        },
+        {
+            trickSet: butterTricks,
+            title: "Butters",
+            treeId: "butterSkillTree",
+            description: "Skill tree for butter tricks"
+        },
+        {
+            trickSet: stallTricks,
+            title: "Stalls",
+            treeId: "stallSkillTree",
+            description: "Skill tree for stall tricks"
+        },
+        {
+            trickSet: flipTricks,
+            title: "Flips",
+            treeId: "flipSkillTree",
+            description: "Skill tree for flip tricks"
+        }
+    ]
+
+    return (
+        <div className="App">
+            {allTricks.map((item, index) => {
+                return  <SkillProvider key={index}>
+                            <SkillTreeGroup>
+                                {({ skillCount }) => (
+                                    <SkillTree
+                                        treeId={item.treeId}
+                                        title={item.title}
+                                        data={item.trickSet}
+                                        collapsible
+                                        description={item.description}
+                                    />
+                                )}
+                            </SkillTreeGroup>
+                        </SkillProvider>
+            })}
+        </div>
+    );
 }
 
 export default App;
